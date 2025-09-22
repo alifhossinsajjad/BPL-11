ddfb43e5340f64cedfeb50655b4e89924a084d70
MD ALIF MIA <alifhossinsajjad123456@gmail.com>
2025-09-22 14:03:12 +0600

    choose the player btn are dynamic done

--- src/components/AvailablePlayers/AvailablePlayers.jsx
+++ src/components/AvailablePlayers/AvailablePlayers.jsx
@@ -1,6 +1,6 @@
 import React, { use } from 'react';
-import UserImg from '../../assets/user-1.png'
-import countryFlag from '../../assets/report-1.png'
+import PlayerCard from '../PlayerCard/PlayerCard';
+
 
 const AvailablePlayers = ({ playerPromise }) => {
 
@@ -12,45 +12,7 @@ const AvailablePlayers = ({ playerPromise }) => {
         <div className='max-w-10/12 mx-auto my-10 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-4 gap-6'>
 
             {
-                playerData.map(player => <div key={player.id}
-                    className="card bg-base-100 w-80 mx-auto md:w-96 xl:w-96 shadow-sm p-4">
-                    <figure>
-                        <img
-                            src={player.playerImg}
-                            alt="Shoes" 
-                            className='w-full h-62 object-cover'/>
-                    </figure>
-                    <div className="m-4">
-                        <div className='flex items-center gap-3 mt-2 mb-3'>
-                            <img src={UserImg} alt="" />
-                            <h2>{player.playerName}</h2>
-                        </div>
-                        <div className='flex justify-between items-center mb-3 border-b pb-3 border-gray-300'>
-                            <div className='flex items-center '>
-                                <img src={countryFlag} alt="" />
-                                <span>{player.playerCountry}</span>
-                            </div>
-                            <button className="btn">{player.playerDesignation}</button>
-                        </div>
-
-                        <div className='flex justify-between items-center mb-3'>
-                            <span>Rating :</span>
-                            <span>{player.playerRating}</span>
-                        </div>
-
-                        <div className='flex justify-between items-center mb-3'>
-                            <span>{player.playerHand}</span>
-                            <span>{player.playerRole}</span>
-                        </div>
-
-                        <div className="card-actions mt-4 flex justify-between items-center">
-                            <p>Price: {player.
-                                playerBrandValue
-                            }</p>
-                            <button className="btn">Choose Player</button>
-                        </div>
-                    </div>
-                </div>)
+                playerData.map(player => <PlayerCard player={player}/>)
             }
 
 
---
+++ src/components/PlayerCard/PlayerCard.jsx
@@ -0,0 +1,55 @@
+
+ import UserImg from '../../assets/user-1.png'
+import countryFlag from '../../assets/report-1.png'
+import { useState } from 'react';
+ 
+ const PlayerCard = ({player}) => {
+
+
+    const [isSelected , setIsSelected] = useState(false)
+
+
+    return (
+        <div key={player.id}
+                    className="card bg-base-100 w-80 mx-auto md:w-96 xl:w-96 shadow-sm p-4">
+                    <figure>
+                        <img
+                            src={player.playerImg}
+                            alt="Shoes" 
+                            className='w-full h-62 object-cover'/>
+                    </figure>
+                    <div className="m-4">
+                        <div className='flex items-center gap-3 mt-2 mb-3'>
+                            <img src={UserImg} alt="" />
+                            <h2>{player.playerName}</h2>
+                        </div>
+                        <div className='flex justify-between items-center mb-3 border-b pb-3 border-gray-300'>
+                            <div className='flex items-center '>
+                                <img src={countryFlag} alt="" />
+                                <span>{player.playerCountry}</span>
+                            </div>
+                            <button className="btn">{player.playerDesignation}</button>
+                        </div>
+
+                        <div className='flex justify-between items-center mb-3'>
+                            <span>Rating :</span>
+                            <span>{player.playerRating}</span>
+                        </div>
+
+                        <div className='flex justify-between items-center mb-3'>
+                            <span>{player.playerHand}</span>
+                            <span>{player.playerRole}</span>
+                        </div>
+
+                        <div className="card-actions mt-4 flex justify-between items-center">
+                            <p>Price: {player.
+                                playerBrandValue
+                            }</p>
+                            <button onClick={() => {setIsSelected(true)}} className="btn">{isSelected === true ? "Selected Player" :"Choose the Player"}</button>
+                        </div>
+                    </div>
+                </div>
+    );
+ };
+ 
+ export default PlayerCard;
